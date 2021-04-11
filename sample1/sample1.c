d33acd0167c5ea0250a06628bac5c16283ece768
Toshihiro Sekioka <toshihiro_sekioka@yayoi-kk.co.jp>
2021-04-11 11:08:15 +0900

    小計と合計の表示

--- sample1/sample1.c
+++ sample1/sample1.c
@@ -5,5 +5,19 @@
 
 int main(int argc, char *argv[])
 {
-	printf("Hello World");
+    int priceA = 100;
+    int amountA = 5;
+
+    int priceB = 200;
+    int amountB = 10;
+    
+    int summary = 0;
+
+    printf("小計(A) %d * %d = %d\n", priceA, amountA, priceA * amountA);
+    summary += priceA * amountA;
+
+    printf("小計(B) %d * %d = %d\n", priceB, amountB, priceB * amountB);
+    summary += priceB * amountB;
+
+    printf("合計 %d", (int)(summary * 1.08f));
 }
