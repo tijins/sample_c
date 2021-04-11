0b7a874d4017f7ccf33d9c29b61564ac088492b2
Toshihiro Sekioka <toshihiro_sekioka@yayoi-kk.co.jp>
2021-04-11 22:28:27 +0900

    ポインタの説明を追加

--- sample1/sample1.c
+++ sample1/sample1.c
@@ -5,5 +5,25 @@
 
 int main(int argc, char *argv[])
 {
-	printf("Hello World");
+	int array[3] = {1,2,3};
+	int num = 1;
+	int* p_num = &num;
+	printf("num %p=%d\n", &num, num);// &変数名 で numのポインタに変換できる
+	*p_num = 2;// *ポインタ名で、ポインタが指す先の変数にアクセスできる
+	printf("p_num %p=%d\n", p_num, num);// &変数名 で numのポインタに変換できる
+	printf("\n");
+
+	printf("array %p\n", array);//配列名は、配列の先頭要素のポインタ
+	printf("array %p=%d\n", &array[0], array[0]);
+	printf("array %p=%d\n", &array[1], array[1]);
+	printf("array %p=%d\n", &array[2], array[2]);
+	printf("\n");
+
+	array[0] = 10;
+	printf("array %p=%d\n", &array[0], array[0]);
+
+	array[1] = 20;
+	*(array+1) = 20;//array[1]と*(array+1)は同じ意味
+	printf("array %p=%d\n", &array[1], array[1]);
+	printf("array %p=%d\n", array+1, *(array+1));
 }
