62516eba966bffebe04a89064c73bf852dff7660
Toshihiro Sekioka <toshihiro_sekioka@yayoi-kk.co.jp>
2021-04-12 20:51:09 +0900

    値渡しと参照渡しを追加

--- sample1/sample1.c
+++ sample1/sample1.c
@@ -3,6 +3,8 @@
 
 #include<stdio.h>
 
+int sampleFunc(int value, int* pValue);
+
 int main(int argc, char *argv[])
 {
 	int array[3] = {1,2,3};
@@ -26,4 +28,16 @@ int main(int argc, char *argv[])
 	*(array+1) = 20;//array[1]と*(array+1)は同じ意味
 	printf("array %p=%d\n", &array[1], array[1]);
 	printf("array %p=%d\n", array+1, *(array+1));
+
+	//値渡しと参照渡し
+	int valueA = 10;
+	int valueB = 10;
+	sampleFunc(valueA, &valueB);
+	printf("valueA=%d, valueV=%d", valueA, valueB);
+}
+
+int sampleFunc(int value, int* pValue)
+{
+	value = value + 1;
+	*pValue = *pValue + 1;
 }
