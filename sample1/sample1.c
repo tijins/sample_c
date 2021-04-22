086b3b4dbf45a03bd2b32ff215b1411130b5ef92
Toshihiro Sekioka <toshihiro_sekioka@yayoi-kk.co.jp>
2021-04-22 17:01:28 +0900

    構造体のポインタ

--- sample1/sample1.c
+++ sample1/sample1.c
@@ -8,32 +8,25 @@
 
 #define MEMBER_MAX 3
 
+void inputPerson(struct Person* person);
+
 int main(int argc, char *argv[])
 {
 	struct Person members[MEMBER_MAX];
-	char nameTemp[sizeof(members[0].name)];
-	char lastTemp[2];
-	int ageTemp;
-	int count = 0;
 	int i;
+	int memberCount;
 
-	while (count < MEMBER_MAX)
+	i = 0;
+	while (i < MEMBER_MAX)
 	{
-
-		printf("name ?>");
-		scanf_s("%s", nameTemp, sizeof(nameTemp));
-
-		printf("age ?>");
-		scanf_s("%d", &ageTemp, sizeof(ageTemp));
-
+		char lastTemp[2];
+		inputPerson(&members[i]);
+		
 		printf("finish (Y=end) ?>");
 		scanf_s("%s", lastTemp, sizeof(lastTemp));
 
-		memset(&members[count], 0, sizeof(members[count]));
-		strncpy_s(members[count].name, sizeof(members[count].name), nameTemp, sizeof(nameTemp));
-		members[count].age = ageTemp;
-		
-		count++;
+		i++;
+		memberCount = i;
 
 		// 途中で終わる
 		if (lastTemp[0] == 'Y') {
@@ -41,9 +34,26 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	for (i = 0; i < count; i++)
+	for (i = 0; i < memberCount; i++)
 	{
 		printf("name=%s age=%d \n", members[i].name, members[i].age);
 	}
 
+}
+
+void inputPerson(struct Person* person)
+{
+	char nameTemp[sizeof(person->name)];
+	int ageTemp;
+
+	printf("name ?>");
+	scanf_s("%s", nameTemp, sizeof(nameTemp));
+
+	printf("age ?>");
+	scanf_s("%d", &ageTemp);
+
+
+	memset(person, 0, sizeof(*person));
+	strncpy_s(person->name, sizeof(person->name), nameTemp, sizeof(nameTemp));
+	person->age = ageTemp;
 }
