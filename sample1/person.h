0e677ff8be84905e8a5ae7cfa2777f560a51cfea
Toshihiro Sekioka <toshihiro_sekioka@yayoi-kk.co.jp>
2021-04-22 16:32:36 +0900

    構造体のサンプル

---
+++ sample1/person.h
@@ -0,0 +1,8 @@
+#include<stdint.h>
+
+struct Person
+{
+	char name[64];
+	char address[64];
+	int age;
+};
--- sample1/sample1.c
+++ sample1/sample1.c
@@ -2,8 +2,28 @@
 //
 
 #include<stdio.h>
+#include<string.h>
+
+#include "person.h"
 
 int main(int argc, char *argv[])
 {
-	printf("Hello World");
+	struct Person yamada;
+	struct Person tanaka;
+
+	memset(&yamada, 0, sizeof(yamada));
+	
+	// strncpyは非推奨なので、コンパイラのエラーを無効にしています
+	#pragma warning(suppress : 4996) 
+	strncpy(yamada.name, "Yamada Taro", sizeof(yamada.name));
+	yamada.age = 20;
+
+
+	memset(&tanaka, 0, sizeof(tanaka));
+	strncpy_s(tanaka.name, sizeof(tanaka.name), "Tanaka Jiro", sizeof("Tanaka Jiro"));
+	tanaka.age = 10;
+
+	printf("name=%s age=%d \n", yamada.name, yamada.age);
+	printf("name=%s age=%d \n", tanaka.name, tanaka.age);
+
 }
